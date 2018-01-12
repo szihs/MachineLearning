2a93018fb6ade66ea45cd6c2a4666fbc7f534f3c
Harsh Aggarwal <harsh1492@gmail.com>
2018-01-12 07:59:45 +0530

    solve 1: assign 2

--- Assignments/machine-learning-ex2/ex2/plotData.m
+++ Assignments/machine-learning-ex2/ex2/plotData.m
@@ -10,16 +10,27 @@ figure; hold on;
 % Instructions: Plot the positive and negative examples on a
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
-%
+%   
 
 
+% M = size(X, 1);
+% for ind = 1:M
+%     if y(ind) == 0
+%         plot(X(ind,1), X(ind,2), 'yo-');
+%     else
+%         plot(X(ind,1), X(ind,2), 'k+-');
+%     end
+% end
+% xlabel('Exam 1 score');
+% ylabel('Exam 2 score');
+% legend('Admitted', 'Not Admitted');
 
 
-
-
-
-
-
+pos = find(y==1); neg = find(y == 0);
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, ...
+'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...
+'MarkerSize', 7);
 % =========================================================================
 
 
